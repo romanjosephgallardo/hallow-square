e7ac80b621ed5687d61d4d5d740c996829b45a94
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 17:42:25 +0800

    Define a function from the loops.

--- hallow_square.py
+++ hallow_square.py
@@ -1,19 +1,19 @@
 # Hallow Square
 
+# Define the function to generate the square
+def hallow_square(n):
+    # Upper side of the square
+    for upper in range(n):
+        print("x", end="")
+    # Space between the upper and lower side of the square
+    print()
+    # Generate the horizontal side of the square
+    for horizontal_sides in range(n - 2):
+        print("x" + (" " * (n - 2)) + "x")
+    # Lower side of the square
+    for lower in range(n):
+        print("x", end="")
+
 # User input for the side length of the square
 length_n = int(input("Enter the side length of the square: "))
-
-# Loop to generate the square
-# Upper side of the square
-for upper in range(length_n):
-    print("x", end="")
-
-print()
-
-# Generate the horizontal side of the square
-for horizontal_sides in range(length_n - 2):
-    print("x" + (" " * (length_n - 2)) + "x")
-
-# Lower side of the square
-for lower in range(length_n):
-    print("x", end="")
+hallow_square(length_n) # Call the function
