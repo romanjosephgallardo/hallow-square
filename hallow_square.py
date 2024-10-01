b7d292301e14a7a5a111651aecc6431a0ce8a4b1
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2024-10-01 17:09:08 +0800

    Add pseudocode

---
+++ hallow_square.py
@@ -0,0 +1,17 @@
+'''
+Generate a hollow square based on the + integer input side length n.
+
+Enter the side length of the square: 6
+xxxxxx
+x    x
+x    x
+x    x
+x    x
+xxxxxx
+
+'''
+
+# pseudocode
+# User input for the side length of the square
+# Loop to generate the square
+# Generate the hollow square
